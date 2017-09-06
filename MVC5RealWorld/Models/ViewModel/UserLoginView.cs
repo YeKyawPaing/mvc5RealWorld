9d9fdb2adecfbf57c073d66a8e70c3d212adb385
YeKyawPaing <wiz.ash.1331@gmail.com>
2017-09-06 17:31:02 +0800

    Add project files.

---
+++ MVC5RealWorld/Models/ViewModel/UserLoginView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC5RealWorld.Models.ViewModel
+{
+    public class UserLoginView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
